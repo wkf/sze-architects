43928e02a4ac5db45c002a554ade3f7da0ad3336
Will Farrell <will@mojotech.com>
2015-08-22 13:39:58 -0400

    Update base url.

--- src/main/clj/sze_architects/site.clj
+++ src/main/clj/sze_architects/site.clj
@@ -10,7 +10,7 @@
   {:pretty-print? false})
 
 (def markup-manifest-config
-  {:base "/sze-architects"
+  {:base "http://willfarrell.is/sze-architects/"
    :styles ["css/out/screen.css"
             "//fast.fonts.net/cssapi/f5a4bdc2-eb4a-40ed-b8fa-c53138c27cc2.css"
             "//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"]
