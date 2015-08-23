0ca49387bd07b70be07337854fddd85a8ccaef0b
Will Farrell <will@mojotech.com>
2015-08-23 19:24:10 -0400

    Vendor cljsjs dependencies until they are packaged

--- project.clj
+++ project.clj
@@ -8,14 +8,17 @@
   :main sze-architects.site
   :source-paths ["src/main/clj"
                  "src/main/cljs"]
+  :resource-paths ["resources"
+                   "lib/dropkick-2.1.7-0.jar"
+                   "lib/fastclick-1.0.6-0.jar"]
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [org.clojure/clojurescript "0.0-3308"]
                  [aviary "0.1.17"]
                  [enlive "1.1.5"]
                  [garden "1.2.5"]
                  [org.jodd/jodd-lagarto "3.6.6"]
-                 [cljsjs/dropkick "2.1.7-0"]
-                 [cljsjs/fastclick "1.0.6-0"]]
+                 #_[cljsjs/dropkick "2.1.7-0"]
+                 #_[cljsjs/fastclick "1.0.6-0"]]
   :profiles {:dev {:source-paths ["src/dev/clj"
                                   "src/dev/cljs"]
                    :dependencies [[weasel "0.5.0"]
